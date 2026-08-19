2d1a6255ce22deb6c5736bdfee050b469c00b824
BryanTamrin <153815751+BryanTamrin@users.noreply.github.com>
2026-08-19 09:09:59 +0000

    ChromaDB implemented

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 from pypdf import PdfReader
 from pathlib import Path
+import ollama
+import chromadb
 
 def main():
     # check input
@@ -13,13 +15,27 @@ def main():
     
     # read file
     reader = PdfReader(sys.argv[1])
-    extracted_text = []
+    extracted_page = []
     for page in reader.pages:
         text = (page.extract_text())
-        extracted_text.append(text)
-    text = "\n".join(extracted_text)
-    print(text)
+        extracted_page.append(text)
 
-    
+    # convert to numbers
+    page_embedding = []
+    for page in extracted_page:
+        response = ollama.embeddings(model="nomic-embed-text", input=page)
+        page_embedding.append(response['embedding'])
+
+    client = chromadb.PersistentClient(path="./chroma_db")
+    collection = client.get_or_create_collection("pdf_gpt")
+
+    for i, page in enumerate(extracted_page):
+        n_of_page = i + 1
+        collection.add(
+            documents=[page],
+            ids=[f"{sys.argv[1]}_page_{n_of_page}"],
+            metadatas=[{"source": sys.argv[1], "page": n_of_page}],
+            embeddings=[page_embedding[i]]
+        )
 if __name__ == "__main__":
     main()
