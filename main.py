180eec64d02f6a2fa51f5508da3085858af43fa0
BryanTamrin <153815751+BryanTamrin@users.noreply.github.com>
2026-08-20 11:25:53 +0000

    Core functions added

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def main():
     # convert to numbers
     page_embedding = []
     for page in extracted_page:
-        response = ollama.embeddings(model="nomic-embed-text", input=page)
+        response = ollama.embeddings(model="nomic-embed-text", prompt=page)
         page_embedding.append(response['embedding'])
 
     client = chromadb.PersistentClient(path="./chroma_db")
@@ -37,5 +37,26 @@ def main():
             metadatas=[{"source": sys.argv[1], "page": n_of_page}],
             embeddings=[page_embedding[i]]
         )
+
+    print(f"{sys.argv[1]} has been added to the database")
+
+    # process user question
+    question = input(f"\n What would you like to ask about {sys.argv[1]}? ")
+    question_embedding = ollama.embeddings(model="nomic-embed-text", prompt=question)["embedding"]
+    results = collection.query(
+        query_embeddings=[question_embedding],
+        n_results=3
+    )
+    retrieved_text = results['documents'][0]
+    if not retrieved_text:
+        print("No relevant information found in the PDF.")
+        return
+
+    total_text = " ".join(retrieved_text)
+    prompt = f"Answer the following question based on the provided text. If the text does not contain the answer, respond with 'I don't know'.\n\nText: {total_text}\n\nQuestion: {question}\n\nAnswer:"
+    response = ollama.generate(model="gemma3:1b", prompt=prompt)
+    print(response['response'])
+
+
 if __name__ == "__main__":
     main()
