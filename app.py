7b7ee57288986f7db9c75109cf3624f18af00242
BryanTamrin <153815751+BryanTamrin@users.noreply.github.com>
2026-08-28 10:13:18 +0000

    Improved PDF list

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
 from function import pdf_to_page, page_to_embedding, add_database, process_question
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, UploadFile
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
+import os
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def home(request: Request):
+    pdfs = os.listdir("pdfs")
     return templates.TemplateResponse(
         request,
-        "index.html"
-        )
+        "index.html",
+        {"pdfs": pdfs}
+    )
+@app.post("/upload")
+async def upload_pdf(file: UploadFile):
+    content = await file.read()
+
+    with open (f"pdfs/{file.filename}", "wb") as f:
+        f.write(content)
+    return RedirectResponse(url="/", status_code=303)
+
+@app.post("/delete")
+async def delete_pdf(pdf_name: str):
+    os.remove(f"pdfs/{pdf_name}")
+    return RedirectResponse(url="/", status_code=303)
